d467123f25f8e72e9646c988b5f35e7474fda30c
Yunjung Jo <j_yunj@naver.com>
2023-01-17 15:43:08 +0900

    first commit

---
+++ jQuery/app.js
@@ -0,0 +1,7 @@
+$(function () {
+  // jQuery methods go here...
+  const title = $('.title').text()
+  console.log(title)
+  //링크를 바디 하단이 아닌 헤드부분에 둬도 이 괄호안에 넣으면 작동 가능
+  //html문서를 다 준비하고 여기부분을 읽어라는 듯
+})
